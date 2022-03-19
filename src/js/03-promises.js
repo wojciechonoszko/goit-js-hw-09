6c6b37c9bc2a23ddcff1ca76325b7d40dd166d11
wojciechonoszko <wojciechonoszko@gmail.com>
2022-03-19 11:18:22 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,12 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+const position = 
+
+const amount = document.querySelector('[name = "amount"]').value
+console.log(amount);
+const btnCreatePromises = document.querySelector('[type = "submit"]');
+console.log(btnCreatePromises);
+
+btnCreatePromises.addEventListener("click", createPromise())
