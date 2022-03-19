ddd91e64cdbb3e8d997fa20c748e04c397d69f19
wojciechonoszko <wojciechonoszko@gmail.com>
2022-03-19 22:04:01 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,45 @@
-function createPromise(position, delay) {
+// function createPromise(position, delay) {
+  
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//     window.alert("Allright!")
+//   } else {
+//     // Reject
+//     window.alert("No good!")
+//   }
+// }
+
+const promise = new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  const delay = parseInt(document.querySelector('[name = "delay"]').value, 10);
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve("Success!");
+    } else {
+      reject("Error!");
+    }
+  }, delay );
+});
 
-const position = 
+promise
+.then(value => {
+  console.log(value);
+})
+.catch(error => {
+  console.log(error);
+});
 
-const amount = document.querySelector('[name = "amount"]').value
+
+
+
+
+
+const delayStep = document.querySelector('[name = "step"]').value;
+
+const amount = document.querySelector('[name = "amount"]').value;
 console.log(amount);
 const btnCreatePromises = document.querySelector('[type = "submit"]');
 console.log(btnCreatePromises);
 
-btnCreatePromises.addEventListener("click", createPromise())
+// btnCreatePromises.addEventListener("click", Promise())
