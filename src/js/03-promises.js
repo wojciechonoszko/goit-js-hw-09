d9ed9b2b53ca0887ec73c6c3ac151a02d9383716
wojciechonoszko <wojciechonoszko@gmail.com>
2022-03-20 21:53:23 +0100

    updating

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,45 +1,65 @@
-// function createPromise(position, delay) {
-  
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//     window.alert("Allright!")
-//   } else {
-//     // Reject
-//     window.alert("No good!")
-//   }
-// }
 
-const promise = new Promise((resolve, reject) => {
-  const shouldResolve = Math.random() > 0.3;
-  const delay = parseInt(document.querySelector('[name = "delay"]').value, 10);
-  setTimeout(() => {
-    if (shouldResolve) {
-      resolve("Success!");
-    } else {
-      reject("Error!");
-    }
-  }, delay );
-});
-
-promise
-.then(value => {
-  console.log(value);
-})
-.catch(error => {
-  console.log(error);
-});
-
-
-
-
-
-
-const delayStep = document.querySelector('[name = "step"]').value;
-
-const amount = document.querySelector('[name = "amount"]').value;
-console.log(amount);
 const btnCreatePromises = document.querySelector('[type = "submit"]');
 console.log(btnCreatePromises);
 
-// btnCreatePromises.addEventListener("click", Promise())
+
+    
+  function createPromise(position, delay) {
+      position = parseInt(document.querySelector('[name = "amount"]').value, 10);
+      console.log(position);
+      delay = parseInt(document.querySelector('[name = "delay"]').value, 10);
+      console.log(delay);
+      let delayStep = parseInt(document.querySelector('[name = "step"]').value, 10);
+      setTimeout(() => {
+      for(let i = 0; i <= position-1; i+=1){
+      array = [];
+      const shouldResolve = Math.random() > 0.3;
+      console.log(shouldResolve);
+      
+
+      
+        if (shouldResolve) {
+          setTimeout(() => {
+          // Fulfill
+          //resolve("OK");
+          const elem = i;
+          
+          array.push(elem);
+          console.log(i+1 + " OK");
+          console.log(delay + delayStep*i)
+          //window.alert("Allright!")
+        }, delay + delayStep*i)
+        
+
+        } 
+      
+        else {
+          setTimeout(() => {
+          // Reject
+          //window.alert("No good!")
+          //reject("No good!");
+          const elem = i;
+          
+          array.push(elem);
+          console.log(i+1 + " No good");
+          console.log(delay + delayStep*i);
+          }, delay + delayStep*i);
+        }
+      }
+      console.log(array)
+      }, delay)
+      
+        };
+
+//console.log(createPromise())
+
+
+btnCreatePromises.addEventListener("click", createPromise());
+
+
+
+// const amount = document.querySelector('[name = "amount"]').value;
+// console.log(amount);
+
+
+
